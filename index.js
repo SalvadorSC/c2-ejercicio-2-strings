f05ffe090e53ad382f0ad9e8b68b21c9c9318443
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-05-26 20:06:30 +0200

    mejorada legibilidad ejercicio

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const a = "camel-case-string";
 const b = "snakeCaseString";
 
-console.log(a)
-console.log(b)
+console.log(a);
+console.log("VVVV");
+
 function convertCamelCase (frase){
-  const x = frase.split("-")
+  const x = frase.split("-");
   for (const i in x){
     const primeraLetra = x[i][0].toUpperCase();
 
-    x[i] = x[i].replace(x[i][0],primeraLetra)
+    x[i] = x[i].replace(x[i][0],primeraLetra);
   }
   x[0] = x[0].replace(x[0][0],x[0][0].toLowerCase())
 
-  console.log(x.join(""))
+  console.log(x.join(""));
 }
 
-convertCamelCase(a)
+convertCamelCase(a);
+
+console.log(b);
+console.log("VVVV");
 
 function convertsnakeCase (palabra){
-  const arr = []
+  const arr = [];
   for (const i in palabra){
     if(palabra[i] === palabra[i].toUpperCase()){
-     arr.push(`-${palabra[i].toLowerCase()}`)
+     arr.push(`-${palabra[i].toLowerCase()}`);
     }else{
-      arr.push(palabra[i])
+      arr.push(palabra[i]);
     }
   }
-  console.log(arr.join(""))
+  console.log(arr.join(""));
 }
 
-convertsnakeCase(b)
+convertsnakeCase(b);
